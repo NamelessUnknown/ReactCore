decc6eba587494d5980689b3f5bcb76b6d8f5c57
Lukasz Debiec <genbolson@gmail.com>
2021-01-09 13:58:35 +0100

    MobX refactor - moving states into 1 shared store

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,18 +1,17 @@
-import React, {FormEvent, useState} from 'react'
+import React, {FormEvent, useContext, useState} from 'react'
 import { Button, Form, Segment } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
+import ActivityStore from "../../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 
 interface IProps {
-  setEditMode: (editMode: boolean) => void;
   activity: IActivity;
-  createActivity: (activity: IActivity) => void;
-  editActivity: (activity: IActivity) => void;
-  submitting: boolean;
 }
 
-const ActivityForm: React.FC<IProps> = ({ setEditMode, activity:initialFormState, createActivity, editActivity, submitting }) => {
-
+const ActivityForm: React.FC<IProps> = ({ activity:initialFormState }) => {
+  const activityStore = useContext(ActivityStore);
+  const {createActivity, editActivity, cancelFormOpen, submitting} = activityStore;
     const initializeForm = () => {
         if (initialFormState) {
           return initialFormState;
@@ -90,9 +89,9 @@ const ActivityForm: React.FC<IProps> = ({ setEditMode, activity:initialFormState
           placeholder='Venue'
           value={activity.venue}
         />
-        <Button floated='right' positive type='submit' content='Submit' />
+        <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
         <Button
-          onClick={() => setEditMode(false)}
+          onClick={cancelFormOpen}
           floated='right'
           type='button'
           content='Cancel'
@@ -102,4 +101,4 @@ const ActivityForm: React.FC<IProps> = ({ setEditMode, activity:initialFormState
   );
 };
 
-export default ActivityForm
+export default observer(ActivityForm)
