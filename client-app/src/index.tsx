b867f128c0f82266f7cbfd4f15a405d13b74062a
Lukasz Debiec <genbolson@gmail.com>
2021-01-24 17:00:42 +0100

    Forms, react final form, revalidate added

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -7,8 +7,12 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
+import "react-widgets/dist/css/react-widgets.css";
 import ScrollToTop from './app/layout/ScrollToTop';
 
+import dateFnsLocalizer from "react-widgets-date-fns";
+
+dateFnsLocalizer();
 export const history = createBrowserHistory();
 
 ReactDOM.render(
